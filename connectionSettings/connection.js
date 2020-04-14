2e1395ab11f900f4e1c49399c4fa1c4e60cecc87
birarekinjal <kinjal.birare@botreetechnologies.com>
2020-04-14 14:18:41 +0530

    -

--- connectionSettings/connection.js
+++ connectionSettings/connection.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const express = require('express');
 const connection = express.Router();
 
-require('dotenv').config({path: './variable.env'});
+
+require('dotenv').config({path: 'variable.env'});
 console.log(process.env.DB_URL);
 
 mongoose.connect(process.env.DB_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
